b2ba1db29e9b49f071d0adddb4f720033f487cfa
Christina Gonzalez <cmgamway@gmail.com>
2019-09-24 22:50:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,14 @@
 class Artist
+<<<<<<< HEAD
   attr_accessor :name
+=======
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @artist = artist
+  end
+>>>>>>> 67627ddc9e5a2b9c5b1192c5a679493e0af8d3fe
 end
